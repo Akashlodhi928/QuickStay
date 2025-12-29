97ea9822f40f06ea2e99140656653785d5efdcab
Akashlodhi <127076693+Akashlodhi928@users.noreply.github.com>
2025-12-29 11:25:13 +0530

    Update AuthContext.jsx

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import React, { Children, createContext, useState } from 'react'
 export const authDataContext = createContext()
 
 function AuthCotext({children}) {
-    let serverUrl = "https://quickstay-backend-2nne.onrender.com"
+    let serverUrl = "https://quickstay-backend1.onrender.com"
     let [loading, setLoading] = useState(false)
 
    let value={
